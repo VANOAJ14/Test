913fbac1e276b25adb335f331af8ef145eda6c8e
vanoaj14 <vanoaj@gmail.com>
2020-11-21 18:52:48 +0530

    employee details

---
+++ src/Org/Tcs/Test/Login/employee.java
@@ -0,0 +1,18 @@
+package Org.Tcs.Test.Login;
+
+public class employee {
+	public static void main (String[]args) {
+		byte id = 20;
+		System.out.println ("The id is"+id);
+		short vehNo =3639;
+		int   logId =332487960;
+		long  accNo =1234567892l;
+	System.out.println("The Information is"+vehNo+"\n"+logId+"\t"+accNo);
+	float credit = 121.131f;
+	double d=1211.3126301;
+	char initial = 'v';
+			String email="vanoaj@gmail.com";
+			boolean  isEmp = true;
+	}
+
+}
